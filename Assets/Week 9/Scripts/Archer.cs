022cd0aaf4e86c1f7900570985263d0a75ae9557
meowmeowidoido <abs.haidarimain@gmail.com>
2024-03-14 10:40:42 -0400

    Task 2 complete: chests only open depending on the selected role

--- Assets/Week 9/Scripts/Archer.cs
+++ Assets/Week 9/Scripts/Archer.cs
@@ -13,5 +13,8 @@ public class Archer : Villager
         base.Attack();
         Instantiate(arrowPrefab, spawnPoint.position,spawnPoint.rotation);
     }
-
+    public override ChestType CanOpen()
+    {
+        return ChestType.Archer;
+    }
 }
