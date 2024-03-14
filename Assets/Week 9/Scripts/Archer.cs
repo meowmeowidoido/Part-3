3f819df40d7b1674f6d1d88557bab636f26778e4
meowmeowidoido <abs.haidarimain@gmail.com>
2024-03-14 10:18:13 -0400

    Made inheritance of the villager class for the archer and merchant and some other cool stuff

---
+++ Assets/Week 9/Scripts/Archer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Villager
+{
+    // Start is called before the first frame update
+    public GameObject arrowPrefab;
+    public Transform spawnPoint;
+        protected override void Attack()
+    {
+        destination=transform.position;
+        base.Attack();
+        Instantiate(arrowPrefab, spawnPoint.position,spawnPoint.rotation);
+    }
+
+}
