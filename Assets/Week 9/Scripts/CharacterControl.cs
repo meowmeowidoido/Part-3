ef322330dd3456e65f55f717e594a168f397c064
Abbas Haidari <haidaabb@shernet.sheridancollege.ca>
2024-03-14 21:06:46 -0400

    Task4b complete: using instance and making the character control into a static variable allowing us to change the text of the character role UI.

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,18 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class CharacterControl : MonoBehaviour
+using TMPro;
+public  class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI displayCharacterRole;
+    public static CharacterControl Instance;
     public static Villager SelectedVillager { get; private set; }
+
+    private void Start()
+    {
+        Instance = this;
+   Instance.displayCharacterRole.text = "None Selected";
+    }
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
         {
-            SelectedVillager.Selected(false);
+            SelectedVillager.Selected(false); 
+            Instance.displayCharacterRole.text = "None Selected";
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+     Instance.displayCharacterRole.text = villager.GetType().Name;
     }
+   
     
 }
